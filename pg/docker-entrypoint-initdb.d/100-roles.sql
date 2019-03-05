f36d11f24c2dad8837d6c3b0bfc1122b7e33f7b7
Shawn Sorichetti <shawn@coloredblocks.com>
2019-03-05 17:08:24 -0500

    Add LOGIN permissions to new roles
    These were missing from the initial commit which causes metacpan and
    metacpan-api to be unable to log into the database. These statements
    could have been `CREATE USER` statements and would have avoided the need
    for `WITH LOGIN`, but the end result is the same now.

--- pg/docker-entrypoint-initdb.d/100-roles.sql
+++ pg/docker-entrypoint-initdb.d/100-roles.sql
@@ -1,5 +1,5 @@
-CREATE ROLE metacpan;
-CREATE ROLE "metacpan-api";
+CREATE ROLE metacpan WITH LOGIN;
+CREATE ROLE "metacpan-api" WITH LOGIN;
 
 -- make things easier for when we're poking around from inside the container
 CREATE USER root;
